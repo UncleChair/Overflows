c8fc64e6b4427703e4bb4a52e096b43f4ab2c688
UncleChair <voidgun1998@gmail.com>
2024-10-31 10:10:03 +0800

    Feature: add systary

--- backend/go.mod
+++ backend/go.mod
@@ -5,6 +5,7 @@ go 1.22.0
 toolchain go1.23.2
 
 require (
+	github.com/getlantern/systray v1.2.2
 	github.com/gogf/gf/contrib/drivers/pgsql/v2 v2.7.4
 	github.com/gogf/gf/contrib/drivers/sqlite/v2 v2.7.4
 	github.com/gogf/gf/v2 v2.7.4
@@ -17,9 +18,16 @@ require (
 	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/fatih/color v1.18.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/getlantern/context v0.0.0-20190109183933-c447772a6520 // indirect
+	github.com/getlantern/errors v0.0.0-20190325191628-abdb3e3e36f7 // indirect
+	github.com/getlantern/golog v0.0.0-20190830074920-4ef2e798c2d7 // indirect
+	github.com/getlantern/hex v0.0.0-20190417191902-c6586a6fe0b7 // indirect
+	github.com/getlantern/hidden v0.0.0-20190325191715-f02dbb02be55 // indirect
+	github.com/getlantern/ops v0.0.0-20190325191751-d70cb0d6f85f // indirect
 	github.com/glebarez/go-sqlite v1.22.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/grokify/html-strip-tags-go v0.1.0 // indirect
@@ -30,6 +38,7 @@ require (
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	go.opentelemetry.io/otel v1.31.0 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
 	_ "overflows/internal/packed"
+	"runtime"
+	"time"
 
+	"github.com/getlantern/systray"
+	"github.com/getlantern/systray/example/icon"
+	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gctx"
 
 	"overflows/internal/cmd"
@@ -12,5 +20,62 @@ import (
 )
 
 func main() {
-	cmd.Main.Run(gctx.GetInitCtx())
+	systray.Run(onReady, onExit)
+}
+
+func onReady() {
+	systray.SetTemplateIcon(icon.Data, icon.Data)
+	systray.SetTitle("Overflows")
+	systray.SetTooltip("Overflows")
+
+	go func() {
+		s := g.Server()
+		mUrl := systray.AddMenuItem("Open UI", "my home")
+		mQuit := systray.AddMenuItem("退出", "Quit the whole app")
+
+		// Sets the icon of a menu item. Only available on Mac.
+		// mQuit.SetIcon(icon.Data)
+
+		systray.AddSeparator()
+
+		for {
+			select {
+			case <-mUrl.ClickedCh:
+				port := s.GetListenedPort()
+				err := openURL(fmt.Sprintf("http://127.0.0.1:%d", port))
+				if err != nil {
+					fmt.Println("Failed to open URL:", err)
+				}
+			case <-mQuit.ClickedCh:
+				systray.Quit()
+				s.Shutdown()
+				fmt.Println("Quit2 now...")
+				return
+			}
+		}
+	}()
+
+	go func() {
+		cmd.Main.Run(gctx.GetInitCtx())
+	}()
+}
+
+func onExit() {
+	now := time.Now()
+	os.WriteFile(fmt.Sprintf(`on_exit_%d.txt`, now.UnixNano()), []byte(now.String()), 0644)
+}
+
+func openURL(url string) error {
+	var err error
+	switch runtime.GOOS {
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	default:
+		return fmt.Errorf("unsupported platform")
+	}
+	return err
 }
