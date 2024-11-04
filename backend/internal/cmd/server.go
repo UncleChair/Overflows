8126c9ae5596e83c151c876594c6138a8c4c3f56
VoidGun <zhongnikan@163.com>
2024-11-04 22:33:47 +0800

    Feature: add server config

--- backend/internal/cmd/server.go
+++ backend/internal/cmd/server.go
@@ -2,11 +2,14 @@ package cmd
 
 import (
 	"context"
+	"overflows/api"
 	"overflows/internal/controller/auth"
 	"overflows/internal/controller/users"
+	"overflows/internal/service"
 
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
+	"github.com/gogf/gf/v2/net/goai"
 	"github.com/gogf/gf/v2/os/gcmd"
 )
 
@@ -16,6 +19,20 @@ var (
 		Brief: "start http server backend only",
 		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
 			s := g.Server()
+			openapi := s.GetOpenApi()
+			openapi.Config.CommonResponse = api.CommonRes{}
+			openapi.Config.CommonResponseDataField = `Data`
+			openapi.Info = goai.Info{
+				Title:       "Overflows API Reference",
+				Description: "Overflows",
+			}
+			s.Use(
+				service.Middleware().Ctx,
+				service.Middleware().CORS,
+				service.Middleware().ResponseHandler,
+			)
+			s.EnableAdmin("/system")
+			s.BindMiddleware("/system/*", service.Middleware().Ctx)
 			s.Group("/", func(group *ghttp.RouterGroup) {
 				group.Middleware(ghttp.MiddlewareHandlerResponse)
 				group.Bind(
