460437e3fe7ce09d091e15abcee7473a1fdf1b0f
UncleChair <voidgun1998@gmail.com>
2024-11-05 09:37:37 +0800

    Feature: change static file path to ./

--- backend/internal/consts/system.go
+++ backend/internal/consts/system.go
@@ -5,7 +5,7 @@ const (
 	Version            = "v0.0.1"                              // Server version
 	IVKey              = "rpFObp7BcJ2t7wgp"                    // AES key
 	JWTKey             = "9z6t1iRvyC8DGbFB"                    // JWT key
-	PublicPath         = "/var/www/public"                     // Public storage path
+	PublicPath         = "./"                                  // Public storage path
 	AvatarFolder       = "avatars"                             // Avatar storage folder
 	AvatarPath         = PublicPath + "/" + AvatarFolder       // Avatar storage path
 	ProjectCoverFolder = "project_covers"                      // Project cover storage folder
