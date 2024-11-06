fca37a807f6466cad17faeee27faef05d10a8792
VoidGun <zhongnikan@163.com>
2024-11-06 22:43:30 +0800

    Pref: improve initial process

--- backend/internal/cmd/main.go
+++ backend/internal/cmd/main.go
@@ -2,7 +2,11 @@ package cmd
 
 import (
 	"context"
+	"errors"
+	"overflows/internal/service"
 
+	"github.com/gogf/gf/v2/database/gdb"
+	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gcmd"
 )
 
@@ -15,8 +19,31 @@ var (
 			{Name: "mode", Short: "m", Brief: "run mode, choose from [standalone, server]", IsArg: false},
 		},
 		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
-			Init.Run(ctx)
 			mode := parser.GetOpt("m", "standalone")
+			port := parser.GetOpt("p", 8000)
+			s := g.Server()
+			s.SetPort(port.Int())
+
+			if mode.String() == "standalone" || mode.String() == "server" {
+				gdb.SetDefaultGroup(mode.String() + "Mode")
+			} else {
+				return errors.New("invalid mode")
+			}
+
+			// Do migration for standalone mode only
+			if mode.String() == "standalone" {
+				err = service.SQLiteMigration(ctx)
+				if err != nil {
+					g.Log().Error(ctx, "Database migration failed:", err)
+					return err
+				}
+			}
+
+			// Init Casbin
+			if mode.String() == "standalone" || mode.String() == "server" {
+				service.InitCasbin(ctx, mode.String()+"Mode")
+			}
+
 			if mode.String() == "standalone" {
 				Standalone.Run(ctx)
 			} else if mode.String() == "server" {
